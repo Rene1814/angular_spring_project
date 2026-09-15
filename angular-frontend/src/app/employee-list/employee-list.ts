e381e631cd37bcb12465a829624c94ac5b69196d
Rene Souza <rne_souza@yahoo.com.br>
2026-09-15 16:36:01 -0300

    Employee list component with dummy data

--- angular-frontend/src/app/employee-list/employee-list.ts
+++ angular-frontend/src/app/employee-list/employee-list.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { Employee } from '../employee';
 
 @Component({
-  imports: [],
+  imports: [NgFor],
   selector: 'app-employee-list',
   styleUrl: './employee-list.css',
   templateUrl: './employee-list.html',
 })
 export class EmployeeList {
   employees: Employee[] = [];
+  ngOnInit(): void {
+    this.employees = [
+      { id: 1, firstName: 'John', lastName: 'Doe', emailId: 'john.doe@example.com' },
+      { id: 2, firstName: 'Jane', lastName: 'Smith', emailId: 'jane.smith@example.com' },
+    ];
+  }
 }
