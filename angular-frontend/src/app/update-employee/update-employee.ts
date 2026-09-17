c1ad5575958a17ace05b2e9dc6a0a4e1572edd15
Rene Souza <rne_souza@yahoo.com.br>
2026-09-17 16:33:33 -0300

    Updtade component improvement

--- angular-frontend/src/app/update-employee/update-employee.ts
+++ angular-frontend/src/app/update-employee/update-employee.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { EmployeeService } from '../employee-service';
 import { Employee } from '../employee';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -16,14 +16,18 @@ export class UpdateEmployee {
   employee: Employee = new Employee();
   constructor (private employeeService: EmployeeService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private changeDetectorRef: ChangeDetectorRef
   ){ }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['id'];
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.employeeService.getEmployeeById(this.id).subscribe(
-      data=> {this.employee = data},
-      error=> console.log(error));    
+      data => {
+        this.employee = data;
+        this.changeDetectorRef.markForCheck();
+      },
+      error => console.log(error));
   }
 
   onSubmit(){
